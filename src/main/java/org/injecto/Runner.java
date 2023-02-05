ef7eda9a9ba82f267a5442f012d87f91c7defcc6
Vladimir Glushenkov <vglushenkov@tunatech.io>
2023-02-05 18:44:55 +0300

    feat: ranking + reactive bidder

--- src/main/java/org/injecto/Runner.java
+++ src/main/java/org/injecto/Runner.java
@@ -6,15 +6,13 @@ import org.injecto.bidder.EvenlyHalfBidder;
 import org.injecto.bidder.RandomBidder;
 import org.injecto.bidder.ReactiveBidder;
 
-import java.util.Arrays;
+import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Supplier;
 import java.util.stream.IntStream;
 
-import static java.lang.Math.max;
-
 public class Runner {
-    public double[] run(int runsNumber, Supplier<Bidder> bidder1Factory, Supplier<Bidder> bidder2Factory) {
+    private static int[] run(int runsNumber, Supplier<Bidder> bidder1Factory, Supplier<Bidder> bidder2Factory) {
         return Arrays.stream(IntStream.range(0, runsNumber).parallel().map(i -> {
                     var turnsNumber = ThreadLocalRandom.current().nextInt(1, 100);
                     var cash = ThreadLocalRandom.current().nextInt(turnsNumber, turnsNumber * 10);
@@ -30,44 +28,75 @@ public class Runner {
                     for (int i = 0; i < counters.length; i++) {
                         accumulator[i] += counters[i];
                     }
-                })).mapToDouble(cnt -> cnt * 100. / runsNumber)
-                .toArray();
+                })).toArray();
     }
 
     public static void main(String[] args) {
-        var runner = new Runner();
         var runsNumber = 1_000_000;
-        Supplier<Bidder> testBidder = ReactiveBidder::new;
-        var opponents = Arrays.asList(
+        List<Supplier<Bidder>> opponents = Arrays.asList(
                 RandomBidder::new,
                 EvenlyBidder::new,
                 EvenlyHalfBidder::new,
-                testBidder
+                ReactiveBidder::new
         );
-        var data = opponents.stream().parallel().map(opponent -> {
-            var percents = runner.run(runsNumber, testBidder, opponent);
-            return new String[]{opponent.get().toString(), String.format("%2.1f", percents[1]), String.format("%2.1f", percents[2]), String.format("%2.1f", percents[0])};
-        }).toArray(String[][]::new);
-        var header = new String[]{testBidder.get().toString() + " vs...", "win%", "loss%", "tie%"};
-        renderTable(header, data);
-    }
-
-    private static void renderTable(String[] header, String[][] data) {
-        var columnWidths = Arrays.stream(header).mapToInt(String::length).toArray();
-        for (String[] row : data) {
-            for (int c = 0; c < row.length; c++) {
-                columnWidths[c] = max(columnWidths[c], row[c].length());
+        var combinations = new ArrayList<int[]>();
+        for (int i = 0; i < opponents.size(); i++) {
+            for (int j = i; j < opponents.size(); j++) {
+                combinations.add(new int[] {i, j});
             }
         }
-        var sb = new StringBuilder();
-        for (var w : columnWidths) {
-            sb.append(String.format("%%%ds", w + 1));
+        var wins = combinations.stream().parallel().map(opponentsIdx -> {
+            var counters = run(runsNumber, opponents.get(opponentsIdx[0]), opponents.get(opponentsIdx[1]));
+            var w = new int[opponents.size()];
+            w[opponentsIdx[0]] = counters[1];
+            w[opponentsIdx[1]] = counters[2];
+            return w;
+        }).reduce((wins1, wins2) -> {
+            for (int i = 0; i < wins1.length; i++) {
+                wins1[i] += wins2[i];
+            }
+            return wins1;
+        }).orElse(new int[opponents.size()]);
+
+        var rank = new TreeSet<RankRecord>(Collections.reverseOrder());
+        for (int i = 0; i < wins.length; i++) {
+            rank.add(new RankRecord(opponents.get(i).get().toString(), wins[i]));
         }
-        sb.append('\n');
-        var format = sb.toString();
-        System.err.format(format, (Object[]) header);
-        for (String[] row : data) {
-            System.err.format(format, (Object[]) row);
+        for (var r : rank) {
+            System.err.println(r);
+        }
+    }
+
+    private static class RankRecord implements Comparable<RankRecord> {
+        String name;
+        int value;
+
+        public RankRecord(String name, int value) {
+            this.name = name;
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            RankRecord that = (RankRecord) o;
+            return value == that.value;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(value);
+        }
+
+        @Override
+        public int compareTo(RankRecord o) {
+            return value - o.value;
+        }
+
+        @Override
+        public String toString() {
+            return "%d %s".formatted(value, name);
         }
     }
 }
--- src/main/java/org/injecto/bidder/ReactiveBidder.java
+++ src/main/java/org/injecto/bidder/ReactiveBidder.java
@@ -2,35 +2,33 @@ package org.injecto.bidder;
 
 import auction.Bidder;
 
+import java.util.Arrays;
+
+import static java.lang.Math.ceil;
 import static java.lang.Math.min;
 
 public class ReactiveBidder implements Bidder {
-    private int ownQuantity;
     private int ownCash;
-    private int opponentCash;
-    private int opponentQuantity;
-    private int leftQuantity;
+    private Window opponentBids;
 
 
     @Override
     public void init(int quantity, int cash) {
-        opponentCash = cash;
         ownCash = cash;
-        leftQuantity = quantity;
+        opponentBids = new Window(5); // TODO better heuristic
     }
 
     @Override
     public int placeBid() {
+        var median = opponentBids.median();
+        var ceil = ceil(median);
         var bid = 0;
-        bid = correctBid(min(ownCash, opponentCash) / turnsLeft() + 1);
-
-//        if (ownQuantity > opponentQuantity) {
-//            bid = correctBid(1);
-//        } else {
-//            bid = correctBid(ownCash / (leftQuantity / 2) + 1);
-//            bid = min(bid, opponentCash + 1);
-//        }
-
+        if (median < ceil) {
+            bid = (int) ceil;
+        } else {
+            bid = (int) (ceil + 1);
+        }
+        bid = correctBid(bid);
         ownCash -= bid;
         return bid;
     }
@@ -39,26 +37,82 @@ public class ReactiveBidder implements Bidder {
         return min(bid, ownCash);
     }
 
-    private int turnsLeft() {
-        return leftQuantity / 2;
-    }
-
     @Override
     public void bids(int own, int other) {
-        opponentCash -= other;
-        if (own == other) {
-            ownQuantity++;
-            opponentQuantity++;
-        } else if (own > other) {
-            ownQuantity += 2;
-        } else {
-            opponentQuantity += 2;
-        }
-        leftQuantity -= 2;
+        opponentBids.add(other);
     }
 
     @Override
     public String toString() {
-        return "ReactiveBidder";
+        return "Reactive";
+    }
+
+    private static class Window {
+        int seqNo;
+        int lowestSeqNo;
+        int[] values;
+        int[] seqNos;
+        int capacity;
+
+        public Window(int size) {
+            values = new int[size];
+            seqNos = new int[size];
+        }
+
+        public void add(int val) {
+            if (capacity >= values.length) {
+                // remove the oldest
+                var removedIdx = removeVal(lowestSeqNo, seqNos);
+                lowestSeqNo++;
+                remove(removedIdx, values);
+                capacity--;
+            }
+            var seqNo = this.seqNo++;
+            var insertionIdx = Arrays.binarySearch(values, 0, capacity, val);
+            if (insertionIdx < 0) {
+                insertionIdx = -(insertionIdx + 1);
+            }
+            insert(insertionIdx, val, values);
+            insert(insertionIdx, seqNo, seqNos);
+            capacity++;
+        }
+
+        public double median() {
+            if (capacity == 0) return 0;
+
+            if (capacity % 2 == 1) {
+                return values[capacity / 2];
+            } else {
+                var i = capacity / 2;
+                return (values[i - 1] + values[i]) * 0.5;
+            }
+        }
+
+        static int removeVal(int val, int[] arr) {
+            int idx = -1;
+            for (int i = 0; i < arr.length; i++) {
+                if (idx >= 0) {
+                    arr[i - 1] = arr[i];
+                } else if (arr[i] == val) {
+                    idx = i;
+                }
+            }
+            arr[arr.length - 1] = 0;
+            return idx;
+        }
+
+        static void remove(int idx, int[] arr) {
+            for (int i = idx + 1; i < arr.length; i++) {
+                arr[i - 1] = arr[i];
+            }
+            arr[arr.length - 1] = 0;
+        }
+
+        static void insert(int idx, int val, int[] arr) {
+            for (int i = (arr.length - 1); i > idx; i--) {
+                arr[i] = arr[i - 1];
+            }
+            arr[idx] = val;
+        }
     }
 }
